b41d230db428f0b2822862117802a4bde4686d5e
Viendimine <rajvishap52@gmail.com>
2025-07-09 17:52:40 +0530

    Updated app.py

--- dashboard_project/CS661-project/app.py
+++ dashboard_project/CS661-project/app.py
@@ -3,14 +3,47 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
+def add_blurred_background(local_image_path=None, remote_image_url=None):
+    if local_image_path:
+        import base64
+        with open(local_image_path, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read()).decode()
+        css = f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/jpeg;base64,{encoded_string}");
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+            background-attachment: fixed;
+        }}
+        .stApp::before {{
+            content: "";
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            backdrop-filter: blur(0px);
+            background-color: rgba(255, 255, 255, 0.3);
+            z-index: -1;
+        }}
+        </style>
+        """
+        st.markdown(css, unsafe_allow_html=True)
+
+
+
+
+
 # Load data
 df = pd.read_csv("C:/Users/Acer/CS661-project/dashboard_project/CS661-project/stock_data.csv")
 
 # Set page title
 st.set_page_config(page_title="Macroeconomic Dashboard", layout="wide")
-st.title("📊 Interactive Macro-Economic Dashboard")
 
 options = [
+    "Home",
     "📈 Closing Price Line Chart",
     "📉 RSI Line Chart",
     "📊 MACD Line Chart",
@@ -22,7 +55,26 @@ options = [
 st.sidebar.header("📂 Select a Visualization")
 selected_option = st.sidebar.radio("Choose a chart to display:", options)
 
-if selected_option == "📈 Closing Price Line Chart":
+if selected_option == "Home":
+   add_blurred_background(local_image_path="assets/stock_image.jpg")
+   st.markdown("<h2 style='color:white'>📊 Interactive Macro-Economic Dashboard</h2>", unsafe_allow_html=True)
+   st.markdown("""
+   <div style='color: white'>
+        ## 🔎 About MarketPulse
+
+**MarketPulse** is a data-driven visual analytics system built for understanding the interaction between **stock prices**, **technical indicators**, **sentiment signals**, and **macroeconomic variables** like **GDP** and **inflation**.
+
+Whether you're an analyst, researcher, or curious learner — MarketPulse helps you explore:
+
+- 📈 Price trends and trading volumes  
+- 📉 Technical indicators like **RSI** and **MACD**  
+- 🧠 News-based **sentiment scores**  
+- 🏛️ Economic indicators such as **GDP growth** and **inflation rate**  
+- 🔍 Correlations across multiple financial signals
+   </div>
+   """, unsafe_allow_html=True)
+
+elif selected_option == "📈 Closing Price Line Chart":
     st.subheader("📈 Closing Price Over Time")
     st.plotly_chart(px.line(df, y='Close', title="Closing Price Over Time"), use_container_width=True)
 
